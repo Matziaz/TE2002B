d082179af3db72cd44cb497016ec56f1401b4f0d
andzega2608 <150101221+andzega2608@users.noreply.github.com>
2025-04-22 17:16:43 -0600

    Update MixColumns.vhd

--- Encryptor/MixColumns.vhd
+++ Encryptor/MixColumns.vhd
@@ -1,11 +1,11 @@
 ----------------------------------------------------------------------------------
--- Company:				ITESM - IRS 2025
--- Author:           Yumee Chung, Ana Coronel, Adrián Márquez, Andrés Zegales
--- Create Date: 		22/04/2025
--- Design Name: 		Mix Columns
+-- Company:		ITESM - IRS 2025
+-- Author:           	Yumee Chung, Ana Coronel, Adrián Márquez, Andrés Zegales
+-- Create Date: 	22/04/2025
+-- Design Name: 	Mix Columns
 -- Module Name:		Mix Columns Module
 -- Target Devices: 	DE10-Lite
--- Description: 		Mix Columns AES - Module
+-- Description: 	Mix Columns AES - Module
 ----------------------------------------------------------------------------------
 
 library ieee;
